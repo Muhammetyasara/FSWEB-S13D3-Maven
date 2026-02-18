9a098808290b434c04cb061676a913b597b384fb
MuhammetAkıtıcı <muhammetyasara@gmail.com>
2026-02-18 16:42:31 +0300

    tasks done

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Person {
+
+    String firstName;
+    String lastName;
+    int age;
+    int height;
+    String hairColor;
+    String eyeColor;
+
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    public Person(String firstName, String lastName, int age,
+                  int height, String hairColor, String eyeColor){
+        this(firstName,lastName,age);
+        this.height = height;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public boolean isTeen(){
+        return age > 12 && age < 20;
+    }
+}
